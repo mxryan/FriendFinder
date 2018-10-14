03bb2a2faf423f24aec87e804b84aa1d91c1b7a1
M Ryan <mxryan@live.com>
2018-10-13 23:23:13 -0400

    initial routes

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+var results = [
+  {
+    name: "Arlan Turtlesniffer",
+    scores: [3, 1, 4, 4, 2, 3, 5, 3, 3, 1]
+  },
+  {
+    name: "Falafel Smith",
+    scores: [1, 1, 4, 5, 4, 4, 5, 4, 1, 1]
+  }
+]
+
+module.exports(results);
